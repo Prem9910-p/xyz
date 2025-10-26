b1e7341370b2cb4cdaf6f9b0693282de5e06e6d1
Prem9910-p <Prem60866@gmail.com>
2025-10-26 21:55:41 +0530

    update

---
+++ src/main/java/com/example/xyz/Config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.example.xyz.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://user-frontend-rouge.vercel.app/") // or your frontend URL after deployment
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+}
